19aa919bdca361b7eb88433dae537f7667e0ebd4
neothemachine <maik.riechert@arcor.de>
2012-09-02 16:33:42 +0200

    tabulation regression (performance)

--- src/ADP/Multi/Tabulation.hs
+++ src/ADP/Multi/Tabulation.hs
@@ -5,16 +5,16 @@ import ADP.Multi.Parser
 
 
 -- four-dimensional tabulation
-table2 :: Int -> RichParser2 a b -> RichParser2 a b
-table2 n (info,q) =
-        (info, 
-          \ z -> (!) $ array ((0,0,0,0),(n,n,n,n))
+table2 :: Array Int a -> RichParser2 a b -> RichParser2 a b
+table2 z (info,q) =
+   let (_,n) = bounds z
+       arr = array ((0,0,0,0),(n,n,n,n))
                       [ ((i,j,k,l),q z (i,j,k,l)) |
                         i <- [0..n]
                       , j <- [i..n]
                       , k <- [0..n]
                       , l <- [k..n]
                       ]
-        )
+   in (info, \ _ -> (!) arr )
         
 -- TODO tabulation with diagonal arrays
--- tests/ADP/Tests/RGExample.hs
+++ tests/ADP/Tests/RGExample.hs
@@ -253,7 +253,7 @@ rgknot yieldAlg rangeAlg algebra inp =
       
   z         = mk inp
   (_,n)     = bounds z  
-  tabulated = table2 n  
+  tabulated = table2 z  
   axiom     = axiom' n z
   
   in axiom s
