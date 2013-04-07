92ad8d8d18ef93d3f637459e16ff696a451e0b4e
neothemachine <maik.riechert@arcor.de>
2013-04-07 18:12:08 +0200

    enable adpfusion benchmarking

--- benchmarks/Benchmarks.hs
+++ benchmarks/Benchmarks.hs
@@ -5,21 +5,24 @@ import ADP.Tests.RGExample as RG
 import ADP.Tests.RGExampleDim2 as RG2
 import ADP.Tests.Nussinov as Nuss
 import ADP.Tests.NussinovExample as Nuss2
+
+-- ADPfusion requires ghc 7.6!
+import BioInf.GAPlike as Nuss3
      
 
 -- run with -o report.html -u report.csv  
 main :: IO ()
 main = defaultMain
           [
-              bgroup "nussinov78 (adp)" (benchArray (Nuss.nussinov78 Nuss.pairmax) inputs),
---            bgroup "nussinov78' (adp)" (benchArray (Nuss.nussinov78' Nuss.pairmax) inputs),
-              bgroup "nussinov78 (adp-multi)" (benchArray (Nuss2.nussinov78 Nuss2.pairmax) inputs)
+              bgroup "nussinov78 (Haskell-ADP)" (benchArray (Nuss.nussinov78' Nuss.pairmax) inputs),
+              bgroup "nussinov78 (adp-multi)" (benchArray (Nuss2.nussinov78 Nuss2.pairmax) inputs),
+              bgroup "nussinov78 (ADPfusion)" (benchArray Nuss3.nussinov78 inputs)
           ]
      where
         longInp = "ggcguaggcgccgugcuuuugcuccccgcgcgcuguuuuucucgcugacuuucagcgggcggaaaagccucggccugccgccuuccaccguucauucuagagcaaacaaaaaaugucagcu"
         veryLongInp = longInp ++ longInp
         
-        inputs = [ (show i, take i veryLongInp) | i <- [0,10..100] ]
+        inputs = [ (show i, take i veryLongInp) | i <- [10,20..100] ]
 
 
 
