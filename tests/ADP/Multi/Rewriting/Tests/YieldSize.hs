e5a9259c5bc14ed835ca880a96135b2ec47a39ba
neothemachine <maik.riechert@arcor.de>
2013-04-11 18:35:47 +0200

    refactor test

--- tests/ADP/Multi/Rewriting/Tests/YieldSize.hs
+++ tests/ADP/Multi/Rewriting/Tests/YieldSize.hs
@@ -2,8 +2,8 @@
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 
 module ADP.Multi.Rewriting.Tests.YieldSize (
-    prop_infoMapSize,
-    prop_infoMapElements,
+    prop_yieldSizeMapSize,
+    prop_yieldSizeMapElements,
     prop_yieldSizeDim2
 ) where
 
@@ -19,31 +19,31 @@ import ADP.Multi.Rewriting.YieldSize
 
 elemCount ParserInfo1{} = 1
 elemCount ParserInfo2{} = 2
-infoMapSize = foldl (\ count info -> count + elemCount info ) 0
+yieldSizeMapSize = foldl (\ count info -> count + elemCount info ) 0
 
-prop_infoMapSize (infos :: [ParserInfo]) = 
-    let infoMap = buildYieldSizeMap infos
-    in Map.size infoMap == infoMapSize infos
+prop_yieldSizeMapSize (infos :: [ParserInfo]) = 
+    let yieldSizeMap = buildYieldSizeMap infos
+    in Map.size yieldSizeMap == yieldSizeMapSize infos
     
-prop_infoMapElements (infos :: [ParserInfo]) =
-    let infoMap = buildYieldSizeMap infos
+prop_yieldSizeMapElements (infos :: [ParserInfo]) =
+    let yieldSizeMap = buildYieldSizeMap infos
         reversed = reverse infos
         withIdx = zip [1..] reversed
         exists (i,ParserInfo1 {minYield=min1,maxYield=max1}) = 
-            Map.member (i,1) infoMap && infoMap Map.! (i,1) == (min1,max1)
+            Map.member (i,1) yieldSizeMap && yieldSizeMap Map.! (i,1) == (min1,max1)
         exists (i,ParserInfo2 {minYield2=(min1,min2),maxYield2=(max1,max2)}) = 
-            Map.member (i,1) infoMap && Map.member (i,2) infoMap &&
-            infoMap Map.! (i,1) == (min1,max1) &&
-            infoMap Map.! (i,2) == (min2,max2)
+            Map.member (i,1) yieldSizeMap && Map.member (i,2) yieldSizeMap &&
+            yieldSizeMap Map.! (i,1) == (min1,max1) &&
+            yieldSizeMap Map.! (i,2) == (min2,max2)
     in all exists withIdx
     
 -- calculates the value of doDetermineYieldSize2 in terms of doDetermineYieldSize1
 prop_yieldSizeDim2 (infos :: [ParserInfo]) =
     forAll (genDim2RewritingFunction infos) $ \ f ->
-    let elemInfo = buildYieldSizeMap infos
-        (left,right) = f (Map.keys elemInfo)
+    let yieldSizeMap = buildYieldSizeMap infos
+        (left,right) = f (Map.keys yieldSizeMap)
         yieldToInfo (minY,maxY) = ParserInfo1 {minYield = minY, maxYield = maxY}
-        parserInfos = map (\(i,j) -> yieldToInfo $ elemInfo Map.! (i,j))
+        parserInfos = map (\(i,j) -> yieldToInfo $ yieldSizeMap Map.! (i,j))
         leftInfos = parserInfos left
         rightInfos = parserInfos right
         leftYield = determineYieldSize1 undefined leftInfos
@@ -62,7 +62,7 @@ _shuffle list samples = shuffle list samples
 
 genDim2RewritingFunction :: [ParserInfo] -> Gen Dim2
 genDim2RewritingFunction infos =
-    let len = infoMapSize infos
+    let len = yieldSizeMapSize infos
     in do split <- choose (0,len)
           samples <- mapM (\i -> choose (0,len-i)) [1..len-1]
           return $ \ l ->
